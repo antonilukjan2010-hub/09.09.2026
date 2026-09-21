7b6d8ca5a01171d636f60842c7eb7edb541afd5b
antonilukjan2010-hub <antonilukjan2010@gmail.com>
2026-09-21 09:00:39 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -5,45 +5,6 @@ function alert2() {
     alert("Poznajesz nas na tej stronie")
 }
 btn.addEventListener("click", alert2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const number1 = parseFloat(document.querySelector(".number1").value)
 const number2 = parseFloat(document.querySelector(".number2").value)
 const btn1 = document.querySelector(".wyślij")
@@ -54,7 +15,7 @@ function dodaj2() {
 const number2 = parseFloat(document.querySelector(".number2").value)
 const btn1 = document.querySelector(".wyślij")
 const div1 = document.querySelector(".div1")
-    const number3 = number1 + number2 
+    const number3 += number1 + number2 
     div1.textContent = number3
 }
 
